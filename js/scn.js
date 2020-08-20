1e0f7f3f8efbfd116a8bb9100293d95c2a8f2d20
wwj-0321 <2844974769@qq.com>
2020-08-20 21:59:14 +0800

    first

---
+++ js/scn.js
@@ -0,0 +1,22 @@
+window.onload=function(){
+	var pic = document.querySelector(".pic");
+    var arrPic = ["img/jd1.jpg", "img/jd2.jpg", "img/jd3.jpg","img/jd4.jpg","img/jd5.jpg"];
+	var num=0;
+	var timer=null;
+	var fn1=function(){
+	 num ++;
+	 if(num>=arrPic.length){
+		num = 0;
+	 }
+	pic.src = arrPic[num];
+	return num;
+    }
+	
+   timer=setInterval(function(){
+	fn1();
+    },2000)
+    
+	
+	
+	
+	}
