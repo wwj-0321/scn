03a2cfce1ef7ea5e13daf5ed263e5fc0a8cd1726
wwj-0321 <2844974769@qq.com>
2020-08-31 21:29:25 +0800

    three commit

--- js/scn.js
+++ js/scn.js
@@ -1,4 +1,5 @@
-window.onload=function(){
+/*window.onload=*/
+$(document).ready(function(){
 	var pic = document.querySelector(".pic");
     var arrPic = ["img/jd1.jpg", "img/jd2.jpg", "img/jd3.jpg","img/jd4.jpg","img/jd5.jpg"];
 	var num=0;
@@ -19,4 +20,4 @@ window.onload=function(){
 	
 	
 	
-	}
+	});
